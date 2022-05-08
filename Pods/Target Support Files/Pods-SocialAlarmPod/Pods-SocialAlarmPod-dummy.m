d3c7b018756bacb698f9146d1c9e616607f51b15
Siddhant Magow <>
2022-05-08 20:01:09 +0530

    Initial Commit

---
+++ Pods/Target Support Files/Pods-SocialAlarmPod/Pods-SocialAlarmPod-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SocialAlarmPod : NSObject
+@end
+@implementation PodsDummy_Pods_SocialAlarmPod
+@end
---
+++ SocialAlarmPod/SocialAlarmPod.h
@@ -0,0 +1,18 @@
+//
+//  SocialAlarmPod.h
+//  SocialAlarmPod
+//
+//  Created by Siddhant Magow on 08/05/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SocialAlarmPod.
+FOUNDATION_EXPORT double SocialAlarmPodVersionNumber;
+
+//! Project version string for SocialAlarmPod.
+FOUNDATION_EXPORT const unsigned char SocialAlarmPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SocialAlarmPod/PublicHeader.h>
+
+
